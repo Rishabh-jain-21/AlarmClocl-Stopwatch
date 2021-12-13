5c8cbafceb4463b0c6b3d18945556ae3494d0fc9
Rishabh Jain <79372016+Rishabh-jain-21@users.noreply.github.com>
2021-12-13 15:32:28 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,66 @@
+
+var start = document.getElementById('start');
+var reset = document.getElementById('reset');
+var stop = document.getElementById('stop');
+var h = document.getElementById("hr");
+var m = document.getElementById("min");
+var s = document.getElementById("sec");
+
+//store a reference to the startTimer variable
+var startTimer = null;
+
+start.addEventListener('click', function () {
+    //initialize the variable
+    function startInterval() {
+        startTimer = setInterval(function () {
+            stopwatch();
+            if (h.value == 0 && m.value == 0 && s.value == 0) {
+                clearInterval(startTimer);
+                alert("ALERT!!!! , TIMEOUT");
+            }
+        }, 1000);
+    }
+    startInterval();
+})
+
+reset.addEventListener('click', function () {
+    h.value = 0;
+    m.value = 0;
+    s.value = 0;
+    //stop the timer after pressing "reset"
+    stopInterval()
+})
+
+function timeout() {
+    alert("timeout");
+    return;
+}
+
+function stopwatch() {
+    if (h.value == 0 && m.value == 0 && s.value == 0) {
+        h.value = 0;
+        m.value = 0;
+        s.value = 0;
+
+
+    } else if (s.value != 0) {
+        s.value--;
+    } else if (m.value != 0 && s.value == 0) {
+        s.value = 59;
+        m.value--;
+    } else if (h.value != 0 && m.value == 0) {
+        m.value = 60;
+        h.value--;
+    }
+    return;
+}
+stop.addEventListener('click', function () {
+    stopInterval(startTimer);
+});
+//stop the function after pressing the reset button, 
+//so the time wont go down when selecting a new time after pressing reset
+function stopInterval() {
+
+    clearInterval(startTimer);
+
+}
